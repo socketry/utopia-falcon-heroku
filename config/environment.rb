8f793f4c17dd761091005186ed81174b930ba301
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-10-27 08:09:36 +1300

    Update traces providers.

--- config/environment.rb
+++ config/environment.rb
@@ -7,40 +7,11 @@ require 'utopia/setup'
 UTOPIA ||= Utopia.setup
 
 require 'async/task'
-require 'trace/provider'
 
-Trace::Provider(Async::Task) do
-	def make_fiber(&block)
-		unless self.transient?
-			parent_context = self.trace_context
-		end
-		
-		Fiber.new do |*arguments|
-			set!
-			
-			self.trace_context = parent_context
-			
-			begin
-				@result = yield(self, *arguments)
-				@status = :complete
-				# Console.logger.debug(self) {"Task was completed with #{@children.size} children!"}
-			rescue Async::Stop
-				stop!
-			rescue StandardError => error
-				fail!(error, false)
-			rescue Exception => exception
-				fail!(exception, true)
-			ensure
-				# Console.logger.debug(self) {"Task ensure $!=#{$!} with #{@children.size} children!"}
-				finish!
-			end
-		end
-	end
-end
-
+require 'traces'
 require 'async/pool'
 
-Trace::Provider(Async::Pool::Controller) do
+Traces::Provider(Async::Pool::Controller) do
 	def acquire
 		trace('async.pool.acquire', attributes: {usage: self.usage_string}) {super}
 	end
